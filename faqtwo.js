6499c51d31dd65a2f5569affc4eca8ba773c343e
ashutoshpawar07 <ashutoshpawar07@gmail.com>
2022-06-30 12:24:05 +0530

    first commit

---
+++ faqtwo.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",()=>{
+const button = document.createElement('button')
+button.innerHTML = "FAQ button click here"
+const section = document.getElementById('myfaqsection')
+section.appendChild(button)
+//comment
+
+
+})
